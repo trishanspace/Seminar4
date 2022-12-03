29f1d80d1ed47d877cc497a81d3a9b53fe9e0893
Nataliia <nataliyatrishechkina@mail.ru>
2022-12-03 12:13:47 +0300

    added code for task3

---
+++ task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// Random random = new Random();
+// int[] UserArray = Array();
+// Console.WriteLine(UserArray);
+
+// int[] Array()
+// {
+//     int[] numbers = new int[8];
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = random.Next(0, 9);
+//     }
+//     return numbers;
+// }
+
+// Выводит ошибку System.Int32
+
+Random random = new Random();
+
+int[] numbers = new int[8];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(0, 9);
+    Console.Write("{0} ", numbers[i]);
+}

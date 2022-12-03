725ff30e9e77cfe7be34640facc8877d8907b1a7
Nataliia <nataliyatrishechkina@mail.ru>
2022-12-03 13:02:48 +0300

    added code for task3

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) 
+//и метод который возводит число A в натуральную степень B. Задание должно быть выполнено в методе. 
+//В методе не должно быть вывода в консоль. Не использовать Math.Pow()
+
+bool isParsed1 = int.TryParse(Console.ReadLine(), out int number1);
+bool isParsed2 = int.TryParse(Console.ReadLine(), out int number2);
+if (!isParsed1 || number1 < 1 || !isParsed2 || number2 < 1)
+{
+    Console.WriteLine("Incorrect");
+}
+
+int count = GetDegreeOfNumber();
+Console.WriteLine(count);
+
+int GetDegreeOfNumber()
+{
+    int count = number1;
+    for (int i = 1; i < number2; i++)
+    {
+        count = count * number1;
+    }
+    return count;
+}

fa196673091ed75107a9dea235b2f3e82445f57f
Nataliia <nataliyatrishechkina@mail.ru>
2022-12-03 12:26:31 +0300

    added code for task2

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if (!isParsed || number < 1)
+{
+    Console.WriteLine("Incorrect");
+}
+
+int sum = GetSumNumber(number);
+Console.WriteLine(sum);
+int GetSumNumber (int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int num = number % 10;
+        number = number / 10;
+        sum = sum + num;
+    }
+    return sum;
+}

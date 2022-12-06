eaaf261fd6acc5ab9c52302d93b547f0742e132b
Nataliia <nataliyatrishechkina@mail.ru>
2022-12-06 11:56:42 +0300

    added code for task3_final

--- task3/Program.cs
+++ task3/Program.cs
@@ -2,27 +2,16 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-// Random random = new Random();
-// int[] Array = GetArrayNubers();
-// Console.Write(Array);
-
-// int[] GetArrayNubers()
-// {
-//     int[] numbers = new int[8];
-//     for (int i = 0; i < numbers.Length; i++)
-//     {
-//         numbers[i] = random.Next(0, 9);
-//     }
-//     return numbers;
-// }
-
-// Выводит System.Int32
-
 Random random = new Random();
+int[] Array = GetArrayNubers();
+Console.Write(Array);
 
-int[] numbers = new int[8];
-for (int i = 0; i < numbers.Length; i++)
+int[] GetArrayNubers()
 {
-    numbers[i] = random.Next(0, 9);
-    Console.Write($"{numbers[i]} ");
+    int[] numbers = new int[8];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next(0, 9);
+    }
+    return numbers;
 }
